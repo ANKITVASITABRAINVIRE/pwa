54f270d44b6f63cef907d369f5da9ea20f15f614
ANKITVASITABRAINVIRE <ankit.vasita@brainvire.com>
2024-04-19 15:00:40 +0530

    new Changes

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,11 +4,13 @@ import { precacheAndRoute } from "workbox-precaching";
 import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
 import { registerRoute } from "workbox-routing";
 
+// Define the current version of the application
+const CURRENT_VERSION = "v1";
+
 // Precache and route all assets defined in the __WB_MANIFEST variable
 precacheAndRoute(self.__WB_MANIFEST);
 
 // Define caching strategies for specific routes
-
 // Network first strategy for requests to 'https://jsonplaceholder.typicode.com'
 registerRoute(
   ({ url }) => url.origin === "https://jsonplaceholder.typicode.com",
@@ -19,16 +21,30 @@ registerRoute(
 registerRoute(/\.(?:js|css|html)$/, new StaleWhileRevalidate());
 
 // Event listener for activating the service worker
-
-// Listen for a message from the main thread to indicate new changes
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "NEW_CHANGES") {
-    // Display an alert to notify the user to reload the page
-    self.registration.showNotification("New Changes Available", {
-      body: "Please reload the page to apply the latest changes.",
-    });
-  }
+self.addEventListener("activate", (event) => {
+  // Check if a new version is available
+  event.waitUntil(checkAndNotify());
 });
+
+// Function to check if a new version is available and send notification
+async function checkAndNotify() {
+  try {
+    // Fetch the current version from the server
+    const response = await fetch("/version.json"); // Assuming version is stored in a JSON file
+    const data = await response.json();
+    const latestVersion = data.version;
+
+    // Compare the current version with the latest version
+    if (latestVersion !== CURRENT_VERSION) {
+      // Display a notification to notify the user to reload the page
+      self.registration.showNotification("New Build Available", {
+        body: "A new version of the application is available. Please reload the page to apply the latest changes.",
+      });
+    }
+  } catch (error) {
+    console.error("Error checking for updates:", error);
+  }
+}
 
 // Event listener for push events
 self.addEventListener("push", (event) => {
@@ -47,12 +63,6 @@ self.addEventListener("notificationclick", (event) => {
 
   // Add logic here to handle what happens when the user clicks the notification
   // For example, you can open a specific URL
-  if (caches) {
-    // Service worker cache should be cleared with caches.delete()
-    caches.keys().then(function (names) {
-      for (let name of names) caches.delete(name);
-    });
-    console.log("reload from cache buster ....");
-  }
+
   event.waitUntil(clients.openWindow(process.env.PUBLIC_URL));
 });
